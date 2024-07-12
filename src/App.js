1b6ef204a63d29472ad8a52cad3006eef1763ed8
esrasumer <esrasumer28@gmail.com>
2024-07-12 09:51:03 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Item from './component/item/item';
 import TimeTracker from './timeTracker';
 
 function App() {
     return (
         <div className="App">
             <TimeTracker />
+            <Item />
         </div>
     );
 }
